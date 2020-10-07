0956e104cbf676967ae2686d5cf6290f34ec83c7
zhanghx0905 <731931282@qq.com>
2020-10-07 15:48:40 +0800

    用户登录 注册

--- users/models.py
+++ users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    ''' 
+    The password attribute of a User object is a string in this format:
+    <algorithm>$<iterations>$<salt>$<hash>
+    '''
+    name = models.CharField(max_length=20, unique=True)
+    pwd = models.CharField(max_length=100)
--- users/views.py
+++ users/views.py
@@ -1,16 +1,46 @@
+from django.contrib.auth.hashers import check_password, make_password
+from django.core.exceptions import ValidationError
 from django.http.response import JsonResponse
-from django.shortcuts import render
+
+from .models import User
+
+
+def gen_response(code: int, data: str):
+    return JsonResponse({
+        'code': code,
+        'data': data
+    }, status=code)
 
 
 def user_register(request):
-    print("register called")
     if request.method == 'POST':
-        print(request.POST)
-    return JsonResponse({"code": 200})
+        name = request.POST.get('name', None)
+        pwd = request.POST.get('password', None)
+        if name is None or pwd is None:
+            return gen_response(400, 'name or password are not given')
+        pwd = make_password(pwd, None)
+
+        user = User(name=name, pwd=pwd)
+        try:
+            user.full_clean()
+            user.save()
+        except ValidationError as e:
+            return gen_response(400, f"Validation Error of user: {e}")
+        return gen_response(200, pwd)
+    return gen_response(405, f'method {request.method} not allowed')
 
 
 def user_login(request):
-    print("login called")
     if request.method == 'POST':
-        print(request.POST)
-    return JsonResponse({"code": 200})
+        name = request.POST.get('name', None)
+        pwd = request.POST.get('password', None)
+        if name is None or pwd is None:
+            return gen_response(400, 'name or password are not given')
+        target_user = User.objects.filter(name=name)
+        if len(target_user) != 1:
+            return gen_response(400, 'the user does not exist')
+        target_user = target_user[0]
+        if not check_password(pwd, target_user.pwd):
+            return gen_response(400, 'the password invalid')
+        return gen_response(200, 'OK')
+    return gen_response(405, f'method {request.method} not allowed')
