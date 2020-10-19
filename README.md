b7c3cff7b305db1500f16ad2b74ed58040c0c90a
zhanghx0905 <731931282@qq.com>
2020-10-19 19:46:51 +0800

    完成api/users部分测试，包括add,exist,list, 完善app测试

--- README.md
+++ README.md
@@ -6,16 +6,34 @@
 pip install -r requirements_dev.txt
 ```
 
-确保本地环境已安装所有依赖
-
-
+确保本地环境已安装所有依赖。
 
 ### Structure
 
 * __doc__ 后端文档，调用相关接口前请仔细阅读！
 * __app__ Core settings for Django.
 * **users** Django App, 用户管理
-* **log** 生成的日志
+* **logs** 生成的日志
 * __pytest.ini__ Configuration for [pytest](https://docs.pytest.org/en/latest/).
 * __requirements.txt__ Package manager with `pip`.
 * __requirements_dev.txt__ Package manager with `pip`, including extra tools for development.
+
+### 附：本地检查代码覆盖率
+
+`.coveragerc` 文件：
+
+```
+[run]
+branch = True
+source = app,users
+[report]
+show_missing = True
+```
+
+命令行执行：
+
+```
+coverage run manage.py test
+coverage report
+```
+
--- users/tests.py
+++ users/tests.py
@@ -1,6 +1,77 @@
 ''' user/test.py '''
+import json
+
 from django.test import TestCase
+
+from .models import User
+from .apps import add_admin
 
 
 class UserTest(TestCase):
     ''' Test for user app '''
+    token = None
+
+    def test_user_model(self):
+        ''' models.User '''
+        add_admin()
+        admin = User.objects.get(username='admin')
+        self.assertEqual(admin.__str__(), 'admin')
+        # admin.generate_jwt_token()
+
+    def test_user_add(self):
+        ''' views.user_add '''
+        path = '/api/user/add'
+
+        illegal_input(self, path)
+
+        paras = {
+            'name': 'hexiao',
+            'password': 'zhanghx',
+            'department': 'thu',
+            'role': ['IT', 'ASSET']
+        }
+        response = self.client.post(path, data=json.dumps(paras), content_type='json')
+        self.assertEqual(response.json()['code'], 200)
+        self.assertEqual(User.objects.filter(username=paras['name']).count(), 1)
+
+        paras['name'] *= 10
+        response = self.client.post(path, data=json.dumps(paras), content_type='json')
+        self.assertEqual(response.json()['code'], 400)
+
+    def test_user_exist(self):
+        ''' views.user_exist '''
+        path = '/api/user/exist'
+
+        illegal_input(self, path)
+        add_admin()
+        response = self.client.post(path, data=json.dumps({'name': 'admin'}), content_type='json')
+        self.assertTrue(response.json()['exist'])
+
+    def test_user_list(self):
+        ''' views.user_list '''
+        path = '/api/user/list'
+
+        response = self.client.post(path)
+        self.assertEqual(response.json()['code'], 405)
+
+        add_admin()
+        response = self.client.get(path)
+        self.assertEqual(response.json()['code'], 200)
+        users_list = response.json()['data']
+        self.assertEqual(len(users_list), 1)
+        self.assertEqual(users_list[0]['name'], 'admin')
+
+    def test_user_login(self):
+        ''' views.user_login '''
+
+    def test_user_info(self):
+        ''' views.user_info '''
+
+
+def illegal_input(self: UserTest, path: str):
+    ''' 对于只接受POST方法的api 测试不合法的输入 '''
+    response = self.client.get(path)
+    self.assertEqual(response.json()['code'], 405)
+
+    response = self.client.post(path)
+    self.assertEqual(response.json()['code'], 201)
