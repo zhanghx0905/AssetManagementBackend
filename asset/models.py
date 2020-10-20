b84b491c252cff98a3b9b18eecea23ca4def690a
yueyang2000 <m18519660881@163.com>
2020-10-20 22:33:30 +0800

    add api doc for asset, model changes. Ref #14

--- asset/models.py
+++ asset/models.py
@@ -4,7 +4,13 @@ from django.db import models
 
 # Create your models here.
 
+
 class Asset(models.Model):
     ''' asset model '''
     nid = models.CharField(max_length=20, unique=True, verbose_name='nid')
-    name = models.CharField(max_length=30, unique=False, verbose_name='用户名')
+    name = models.CharField(max_length=30, verbose_name='用户名')
+    quantity = models.IntegerField(verbose_name='数量')
+    is_quantity = models.BooleanField(verbose_name='资产类型')
+    description = models.CharField(max_length=50, verbose_name='简介')
+    parent = models.CharField(max_length=20, verbose_name='从属资产')
+    
--- asset/views.py
+++ asset/views.py
@@ -2,8 +2,13 @@
 
 # from django.shortcuts import render
 from app.utils import gen_response
+# from .models import Asset
 
 # Create your views here.
+
+
 def asset_list(request):
     '''return an asset list for asset manager'''
+    # if request.method == 'GET':
+    #     all_asset = Asset.objects.all()
     return gen_response(code=200)
