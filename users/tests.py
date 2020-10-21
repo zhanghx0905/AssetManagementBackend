45f9f261c97ba5f24f33316b4ac1bb22d69c72d1
zhanghx0905 <731931282@qq.com>
2020-10-21 16:29:50 +0800

    基本完成users模块的测试用例

--- users/tests.py
+++ users/tests.py
@@ -17,6 +17,12 @@ class UserTest(TestCase):
         user.set_password('zhanghx')
         user.save()
 
+        add_admin()
+        response = self.client.post('/api/user/login',
+                                    data=json.dumps({'username': 'admin', 'password': 'admin'}),
+                                    content_type='json')
+        self.client.cookies['Token'] = response.json()['token']
+
     def illegal_input(self, path: str):
         ''' 对于只接受POST方法的api 测试不合法的输入 '''
         response = self.client.get(path)
@@ -25,19 +31,9 @@ class UserTest(TestCase):
         response = self.client.post(path)
         self.assertEqual(response.json()['code'], 201)
 
-    def admin_login(self):
-        ''' 登录管理员用户 '''
-        add_admin()
-        path = '/api/user/login'
-        paras = {'username': 'admin', 'password': 'admin'}
-        response = self.client.post(path, data=json.dumps(paras), content_type='json')
-        self.client.cookies['Token'] = response.json()['token']
-
     def test_user_list(self):
         ''' views.user_list '''
         path = '/api/user/list'
-
-        self.admin_login()
 
         response = self.client.post(path)
         self.assertEqual(response.json()['code'], 405)
@@ -49,20 +45,22 @@ class UserTest(TestCase):
         ''' views.user_exist '''
         path = '/api/user/exist'
 
-        self.admin_login()
-
         self.illegal_input(path)
 
-        response = self.client.post(path, data=json.dumps({'name': 'admin'}), content_type='json')
+        response = self.client.post(path,
+                                    data=json.dumps({'name': 'admin'}),
+                                    content_type='json')
         self.assertTrue(response.json()['exist'])
 
-        response = self.client.post(path, data=json.dumps({'name': 'nothisman'}), content_type='json')
+        response = self.client.post(path,
+                                    data=json.dumps({'name': 'noone'}),
+                                    content_type='json')
         self.assertFalse(response.json()['exist'])
 
     def test_user_add_delete(self):
         ''' views.user_add '''
         path = '/api/user/add'
-        self.admin_login()
+
         self.illegal_input(path)
 
         paras = {
@@ -100,7 +98,6 @@ class UserTest(TestCase):
         ''' views.user_edit '''
         path = '/api/user/edit'
 
-        self.admin_login()
         self.illegal_input(path)
 
         paras = {
@@ -114,38 +111,78 @@ class UserTest(TestCase):
         admin = User.objects.get(username='admin')
         self.assertEqual(admin.department, paras['department'])
 
-        paras['name'] = 'nothisman'
+        paras['name'] = 'noone'
         response = self.client.post(path, data=json.dumps(paras), content_type='json')
         self.assertEqual(response.json()['code'], 202)
 
     def test_user_lock(self):
         ''' views.user_lock '''
         path = '/api/user/lock'
-        self.admin_login()
 
         self.illegal_input(path)
 
         paras = {
             'username': 'admin',
             'active': False
-        }
-        response = self.client.post(path, data=json.dumps(paras), content_type='json')
-        self.assertEqual(response.json()['code'], 203)
+        }   # admin 不能锁定
+        response = self.client.post(path, data=json.dumps(paras), content_type='json').json()
+        self.assertEqual(response['code'], 203)
 
+        # 正常锁定
         paras['username'] = 'zhanghx'
-        response = self.client.post(path, data=json.dumps(paras), content_type='json')
-        self.assertEqual(response.json()['code'], 200)
+        response = self.client.post(path, data=json.dumps(paras), content_type='json').json()
+        self.assertEqual(response['code'], 200)
         response = self.client.post('/api/user/login',
                                     data=json.dumps({'username': 'zhanghx', 'password': 'zhanghx'}),
                                     content_type='json')
         self.assertEqual(response.json()['status'], 1)
 
+        # 用户不存在
         paras['username'] = 'nosuchman'
         response = self.client.post(path, data=json.dumps(paras), content_type='json')
         self.assertEqual(response.json()['code'], 202)
 
     def test_user_login_logout(self):
         ''' views.user_login '''
+        path = '/api/user/login'
+
+        self.illegal_input(path)
+
+        paras = {
+            'username': 'noone',
+            'password': 'wrong'
+        }   # 测试用户不存在
+        response = self.client.post(path, json.dumps(paras), content_type='json')
+        self.assertEqual(response.json()['status'], 1)
+
+        paras['username'] = 'zhanghx'
+        # 测试密码错误
+        response = self.client.post(path, json.dumps(paras), content_type='json')
+        self.assertEqual(response.json()['status'], 1)
+        # 正常登录在 setUp 中测试，锁定登录在 test_user_lock 中测试
+
+        paras['password'] = 'zhanghx'
+        response = self.client.post(path, json.dumps(paras), content_type='json')
+        self.client.cookies['Token'] = response.json()['token']
+
+        path = '/api/user/logout'
+        response = self.client.get(path)
+        self.assertEqual(response.json()['code'], 405)
+
+        response = self.client.post(path, json.dumps(paras), content_type='json')
+        self.assertEqual(response.json()['status'], 0)
+
+        # 登出后尝试再登出
+        response = self.client.post(path, json.dumps(paras), content_type='json')
+        self.assertEqual(response.json()['status'], 1)
 
     def test_user_info(self):
         ''' views.user_info '''
+        path = '/api/user/info'
+
+        response = self.client.get(path)
+        self.assertEqual(response.json()['code'], 405)
+
+        response = self.client.post(path).json()
+        self.assertFalse(response['status'])
+        self.assertEqual(response['userInfo']['name'], 'admin')
