71a743c45adeb16b83b4483c86b531f80b8158a3
zhanghx0905 <731931282@qq.com>
2020-10-28 22:24:57 +0800

    '启动应用时添加一个旧资产，以展示折旧'

--- users/apps.py
+++ users/apps.py
@@ -29,6 +29,28 @@ def add_admin():
         pass
 
 
+def add_old_asset():
+    ''' 增加一个旧资产，以展示资产折旧 '''
+    from asset.models import Asset, AssetCategory
+    from users.models import User
+    from datetime import timedelta
+    try:
+        if not Asset.objects.filter(name='旧资产').exists():
+            asset = Asset(name='旧资产',
+                          quantity=1,
+                          value=10000,
+                          category=AssetCategory.root(),
+                          type_name='ITEM',
+                          status='IDLE',
+                          service_life=10,
+                          owner=User.admin())
+            asset.save()
+            asset.start_time -= timedelta(366)
+            asset.save()
+    except OperationalError:
+        pass
+
+
 class UsersConfig(AppConfig):
     ''' UsersConfig '''
     name = 'users'
@@ -38,3 +60,4 @@ class UsersConfig(AppConfig):
         super().ready()
         init_department()
         add_admin()
+        add_old_asset()
