08fb925ba9852639f31785a3cbf17f93bfe2f1b2
zhanghx0905 <731931282@qq.com>
2020-11-02 10:41:24 +0800

    去除csrf验证，增加一个测例

--- asset/tests.py
+++ asset/tests.py
@@ -1,12 +1,15 @@
 '''test for app asset'''
-from django.test import TestCase
+import json
+
+from django.test import TransactionTestCase
 
 from app.utils import init_test
+from asset.models import AssetCategory
 from users.apps import add_old_asset
 from .apps import init_category
 
 
-class AssetTest(TestCase):
+class AssetTest(TransactionTestCase):
     ''' Testcases for app asset '''
 
     def setUp(self) -> None:
@@ -20,3 +23,44 @@ class AssetTest(TestCase):
         ''' test for category_tree '''
         response = self.client.get('/api/asset/category/tree')
         self.assertEqual(response.json()['code'], 200)
+
+    def test_category(self):
+        ''' test for category_add category_delete category_edit '''
+        # 增加一类
+        path = '/api/asset/category/add'
+        paras = {"parent_id": 1, "name": "me"}
+        response = self.client.post(path,
+                                    json.dumps(paras),
+                                    content_type='json')
+        self.assertEqual(response.json()['code'], 200)
+
+        response = self.client.post(path,
+                                    json.dumps(paras),
+                                    content_type='json')
+        self.assertEqual(response.json()['code'], 203)
+
+        # 修改类名
+        path = '/api/asset/category/edit'
+        paras = {"id": 2, "name": "new"}
+        response = self.client.post(path,
+                                    json.dumps(paras),
+                                    content_type='json')
+        self.assertEqual(response.json()['code'], 200)
+
+        paras['name'] = AssetCategory.root().name
+        response = self.client.post(path,
+                                    json.dumps(paras),
+                                    content_type='json')
+        self.assertEqual(response.json()['code'], 203)
+
+        # 删除类
+        path = '/api/asset/category/delete'
+        response = self.client.post(path,
+                                    json.dumps({"id": 2}),
+                                    content_type='json')
+        self.assertEqual(response.json()['code'], 200)
+
+        response = self.client.post(path,
+                                    json.dumps({"id": 1}),
+                                    content_type='json')
+        self.assertEqual(response.json()['code'], 203)
