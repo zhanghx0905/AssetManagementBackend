855ad8677f4a4bff9f5f0591be577f5eab01c4bc
zhanghx0905 <731931282@qq.com>
2020-11-02 10:53:10 +0800

    修复测例编写的bug，通过资产类型模块测试

--- asset/tests.py
+++ asset/tests.py
@@ -1,22 +1,23 @@
 '''test for app asset'''
 import json
 
-from django.test import TransactionTestCase
+from django.test import TestCase
+from django.db import transaction
 
 from app.utils import init_test
 from asset.models import AssetCategory
 from users.apps import add_old_asset
-from .apps import init_category
 
 
-class AssetTest(TransactionTestCase):
+class AssetTest(TestCase):
     ''' Testcases for app asset '''
 
     def setUp(self) -> None:
         ''' 加入admin并登录，初始化资产类型
         添加一个旧资产 '''
         init_test(self)
-        init_category()
+        root = AssetCategory(name='资产', parent=None)
+        root.save()
         add_old_asset()
 
     def test_category_tree(self):
@@ -29,38 +30,42 @@ class AssetTest(TransactionTestCase):
         # 增加一类
         path = '/api/asset/category/add'
         paras = {"parent_id": 1, "name": "me"}
-        response = self.client.post(path,
-                                    json.dumps(paras),
-                                    content_type='json')
+        with transaction.atomic():
+            response = self.client.post(path,
+                                        json.dumps(paras),
+                                        content_type='json')
         self.assertEqual(response.json()['code'], 200)
-
-        response = self.client.post(path,
-                                    json.dumps(paras),
-                                    content_type='json')
+        with transaction.atomic():
+            response = self.client.post(path,
+                                        json.dumps(paras),
+                                        content_type='json')
         self.assertEqual(response.json()['code'], 203)
 
         # 修改类名
         path = '/api/asset/category/edit'
         paras = {"id": 2, "name": "new"}
-        response = self.client.post(path,
-                                    json.dumps(paras),
-                                    content_type='json')
+        with transaction.atomic():
+            response = self.client.post(path,
+                                        json.dumps(paras),
+                                        content_type='json')
         self.assertEqual(response.json()['code'], 200)
 
         paras['name'] = AssetCategory.root().name
-        response = self.client.post(path,
-                                    json.dumps(paras),
-                                    content_type='json')
+        with transaction.atomic():
+            response = self.client.post(path,
+                                        json.dumps(paras),
+                                        content_type='json')
         self.assertEqual(response.json()['code'], 203)
 
         # 删除类
         path = '/api/asset/category/delete'
-        response = self.client.post(path,
-                                    json.dumps({"id": 2}),
-                                    content_type='json')
+        with transaction.atomic():
+            response = self.client.post(path,
+                                        json.dumps({"id": 2}),
+                                        content_type='json')
         self.assertEqual(response.json()['code'], 200)
-
-        response = self.client.post(path,
-                                    json.dumps({"id": 1}),
-                                    content_type='json')
+        with transaction.atomic():
+            response = self.client.post(path,
+                                        json.dumps({"id": 1}),
+                                        content_type='json')
         self.assertEqual(response.json()['code'], 203)
